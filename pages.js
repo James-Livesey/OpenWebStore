1addc14576977069ebac3e6b13fa8a101d2656af
James-Livesey <jamesliveg@gmail.com>
2021-04-16 19:41:05 +0100

    Add basic app listing iteration

--- pages.js
+++ pages.js
@@ -1,6 +1,25 @@
 var currentPage = "explore";
 var searchPhrase = "";
 
+var appsList = [
+    {
+        name: "Test App",
+        iconUrl: "",
+        author: {
+            name: "Test User"
+        },
+        description: "test"
+    },
+    {
+        name: "Test 2",
+        iconUrl: "",
+        author: {
+            name: "Test User"
+        },
+        description: "hi"
+    }
+];
+
 function switchToPage(page) {
     currentPage = page;
 
