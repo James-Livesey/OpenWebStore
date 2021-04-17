ab999629986ddf6acbbe495b34fc622a0e16e98f
James-Livesey <jamesliveg@gmail.com>
2021-04-17 16:35:21 +0100

    Add app listing styling and search info

--- pages.js
+++ pages.js
@@ -4,19 +4,38 @@ var searchPhrase = "";
 var appsList = [
     {
         name: "Test App",
-        iconUrl: "",
+        iconUrl: "/media/logo.svg",
         author: {
             name: "Test User"
         },
-        description: "test"
+        description: "An example app for demonstration purposes.",
+        installationUrl: "https://example.com"
     },
     {
-        name: "Test 2",
-        iconUrl: "",
+        name: "GameProxy Chat",
+        iconUrl: "https://gameproxy.host/media/apple-touch-icon.png",
         author: {
-            name: "Test User"
+            name: "Subnodal Technologies"
         },
-        description: "hi"
+        description: "Join our servers now here at GameProxy Chat to start finding your community!",
+        installationUrl: "https://gameproxy.host/chat"
+    },
+    {
+        name: "Proxi",
+        iconUrl: "https://proxi.emux.app/www/media/proxi.svg",
+        author: {
+            name: "Emux Technologies"
+        },
+        description: "Ensure that you keep your distance from others through alerts and tracking.",
+        installationUrl: "https://proxi.emux.app",
+        screenshotUrls: [
+            "https://proxi.emux.app/storefront/screenshot1.png",
+            "https://proxi.emux.app/storefront/screenshot2.png",
+            "https://proxi.emux.app/storefront/screenshot3.png",
+            "https://proxi.emux.app/storefront/screenshot4.png",
+            "https://proxi.emux.app/storefront/screenshot5.png",
+            "https://proxi.emux.app/storefront/screenshot6.png"
+        ]
     }
 ];
 
@@ -29,6 +48,10 @@ function switchToPage(page) {
 function switchToSearch() {
     currentPage = "search";
     searchPhrase = document.getElementById("search").value;
+
+    if (searchPhrase.trim() == "") {
+        currentPage = "explore";
+    }
 
     subElements.render();
 }
